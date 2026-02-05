d2ee7f61e915738793daa6b11aa7145d83ab2166
GryffinX <g.ayush2k07@gmail.com>
2026-02-05 16:47:54 +0530

    Custom user creation logic and user model

---
+++ server/users/managers.py
@@ -0,0 +1,68 @@
+from django.contrib.auth.models import BaseUserManager
+from django.core.exceptions import ValidationError
+
+class CustomUserManager(BaseUserManager):
+    def create_user(
+            self,
+            user,
+            email,
+            username, 
+            organization_name,
+            website,
+            country,
+            password=None,
+            **extra_fields
+    ):
+        if not email:
+            raise ValueError("Email is required")
+        if not organization_name or not website:
+            raise ValueError("Organization name and website are required")
+        email = self.normalize_email(email)
+
+        if self.model.onjects.filter(
+            organization_name_iexact=organization_name.strip(),
+            website_iexact=website.strip()
+        ).exists():
+            raise ValidationError("An account with this organization already exists.")
+        
+        user=self.model(
+            email=email,
+            username=username,
+            organization_name=organization_name,
+            website=website.strip(),
+            country=country,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(
+            self,
+            email,
+            username,
+            organization_name,
+            website,
+            country,
+            password=None,
+            **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_verified", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")  
+        
+        return self.create_user(
+            email,
+            username,
+            organization_name,
+            website,
+            country,
+            password,
+            **extra_fields
+        )
--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,53 @@
 from django.db import models
-
+from .managers import CustomUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 # Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ("sme","Small/Medium Enterprise"),
+        ("ngo","NGO"),
+        ("org", "Large Organization"),
+    )
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(
+        max_length=50,
+        unique=True,
+        help_text="Public handle used in URLs and profiles."
+    )
+
+    organization_name = models.CharField(max_length=255)
+    website = models.URLField()
+    country = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    carbon_balance = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0.00
+    )
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="sme")
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'organization_name', 'website', 'country']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['organization_name', 'website'],
+                name='unique_organization_identity'
+            )
+        ]
